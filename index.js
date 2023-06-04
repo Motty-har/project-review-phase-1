3e3a1ca93c2d9711ba07d4968e52f3bc596a0bd3
Motty-har <harligmotty@gmail.com>
2023-06-04 17:35:21 -0400

    first part of psuedocode and first part of function that creates html to hold player info

---
+++ index.js
@@ -0,0 +1,24 @@
+/*need to create a form that accepts players info and stats and 
+adds it to server using add player button. The player is then added 
+back to the html and visiable on screen.
+There should be a button for goals and assists that can be used to add
+a goal and assists when scored*/
+
+/*ADDING EVENT LISTENER TO THE FORM
+need to create a variable that has access to the form
+add domcontentloaded event listener
+create a funciton that will hold each player info when we want to render it to the screen
+create an event listener on the variable with the form
+prevent devault 
+create the object to hold the info from the form 
+
+*/
+function playerCard(){
+    const playerDiv = document.getElementById("card")
+    console.log(card)
+}
+playerCard()
+
+
+
+
